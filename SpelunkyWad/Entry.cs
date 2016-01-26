67ff122cc6f834e34c3cbbb4c97d25ef550aca8a
Worst-vd-plas <worst.vd.plas@gmail.com>
2016-01-26 20:18:25 +0200

    add spelunkywad

---
+++ SpelunkyWad/Entry.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpelunkyWad
+{
+	/// <summary>
+	/// Represents an entry in a WAD archive.
+	/// </summary>
+	public class Entry
+	{
+		/// <summary>
+		/// Creates a new entry.
+		/// </summary>
+		/// <param name="name">the name</param>
+		/// <param name="data">the data</param>
+		public Entry(string name, byte[] data)
+		{
+			this.Name = name;
+			this.Data = data;
+		}
+
+		/// <summary>
+		/// Returns a string representation of the entry.
+		/// </summary>
+		/// <returns>a string representation</returns>
+		public override string ToString()
+		{
+			return string.Format("Entry (Name: {0}, Data: {1})", this.Name, this.Data);
+		}
+
+		/// <summary>
+		/// Returns if the entry equals another object.
+		/// </summary>
+		/// <param name="obj">the object</param>
+		/// <returns>if this entry equals</returns>
+		public override bool Equals(object obj)
+		{
+			if (obj is Entry)
+			{
+				var entry = (Entry) obj;
+
+				return (this.Name == entry.Name && this.Data.Length == entry.Data.Length);
+			}
+			else
+			{
+				return base.Equals(obj);
+			}
+		}
+
+		/// <summary>
+		/// Returns the hash code for the entry.
+		/// </summary>
+		/// <returns>the hash code</returns>
+		public override int GetHashCode()
+		{
+			return (this.Name.GetHashCode() + this.Data.Length);
+		}
+
+		/// <summary>
+		/// The name of the entry.
+		/// </summary>
+		public string Name
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// The data for the entry.
+		/// </summary>
+		public byte[] Data
+		{
+			get;
+			private set;
+		}
+	}
+}
---
+++ SpelunkyWad/Group.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpelunkyWad
+{
+	/// <summary>
+	/// Represents a group of entries.
+	/// </summary>
+	public class Group
+	{
+		/// <summary>
+		/// Creates a new group.
+		/// </summary>
+		/// <param name="name">the name</param>
+		/// <param name="entries">the entries</param>
+		public Group(string name, List<Entry> entries)
+		{
+			this.Name = name;
+			this.Entries = entries;
+		}
+
+		/// <summary>
+		/// Creates a new group.
+		/// </summary>
+		/// <param name="name">the entries</param>
+		public Group(string name) : this(name, new List<Entry>())
+		{
+
+		}
+
+		/// <summary>
+		/// Returns a string representation of the group.
+		/// </summary>
+		/// <returns>a string representation</returns>
+		public override string ToString()
+		{
+			return string.Format("Group (Name: {0}, Entries: {1})", this.Name, this.Entries);
+		}
+
+		/// <summary>
+		/// Returns if the entry equals another object.
+		/// </summary>
+		/// <param name="obj">the object</param>
+		/// <returns>if the entry equals</returns>
+		public override bool Equals(object obj)
+		{
+			if (obj is Group)
+			{
+				var group = (Group) obj;
+
+				return (this.Name == group.Name && this.Entries == group.Entries);
+			}
+			else
+			{
+				return base.Equals(obj);
+			}
+		}
+
+		/// <summary>
+		/// Returns the hash code for the group.
+		/// </summary>
+		/// <returns>the hash code</returns>
+		public override int GetHashCode()
+		{
+			return (this.Name.GetHashCode() + this.Entries.GetHashCode());
+		}
+
+		/// <summary>
+		/// The name of the group.
+		/// </summary>
+		public string Name
+		{
+			get;
+			private set;
+		}
+
+		/// <summary>
+		/// The entries in the group.
+		/// </summary>
+		public List<Entry> Entries
+		{
+			get;
+			private set;
+		}
+	}
+}
